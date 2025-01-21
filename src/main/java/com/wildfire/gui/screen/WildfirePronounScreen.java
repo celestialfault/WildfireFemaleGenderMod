90e8e50b99f509e4a10e8f9cc5c1cecfe586e8e2
PinguinLars1105 <Heijmen.Lars@gmail.com>
2025-01-21 11:40:57 +0100

    Still working on the UI

--- src/main/java/com/wildfire/gui/screen/WildfirePronounScreen.java
+++ src/main/java/com/wildfire/gui/screen/WildfirePronounScreen.java
@@ -18,22 +18,27 @@
 
 package com.wildfire.gui.screen;
 
+import com.wildfire.gui.GuiUtils;
 import com.wildfire.gui.WildfireButton;
 import com.wildfire.main.WildfireGender;
 import com.wildfire.main.WildfireLocalization;
-import com.wildfire.main.cloud.CloudSync;
+import com.wildfire.main.cloud.SyncLog;
 import com.wildfire.main.config.GlobalConfig;
+import com.wildfire.main.config.enums.Pronoun;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Environment(EnvType.CLIENT)
 public class WildfirePronounScreen extends BaseWildfireScreen {
-    private static final Identifier BACKGROUND = Identifier.of(WildfireGender.MODID, "textures/gui/sync_bg_v2.png");
+    private static final Identifier BACKGROUND = Identifier.of(WildfireGender.MODID, "textures/gui/pronouns_bg.png");
 
     protected WildfirePronounScreen(Screen parent, UUID uuid) {
 		super(Text.translatable("wildfire_gender.cloud_settings"), parent, uuid);
@@ -45,22 +50,72 @@ public class WildfirePronounScreen extends BaseWildfireScreen {
 		int y = this.height / 2;
 		int yPos = y - 47;
 		int xPos = x - 156 / 2 - 1;
-
-        final var ref = new Object() {
-			WildfireButton btnSyncNow, btnAutomaticSync;
-		};
-
+        Pronoun pronoun;
+        String pronouns = switch (pronoun) {
+            case SHE -> "SHE";
+            case HE -> "HE";
+            case THEY -> "THEY";
+            case IT -> "IT";
+        };
         this.addDrawableChild(new WildfireButton(xPos, yPos, 157, 20,
-                Text.translatable("wildfire_gender.pronoun.status", CloudSync.isEnabled() ? WildfireLocalization.ENABLED : WildfireLocalization.DISABLED),
-				button -> {
-					var config = GlobalConfig.INSTANCE;
-					config.set(GlobalConfig.CLOUD_SYNC_ENABLED, !config.get(GlobalConfig.CLOUD_SYNC_ENABLED));
-					button.setMessage(Text.translatable("wildfire_gender.cloud.status", CloudSync.isEnabled() ? WildfireLocalization.ENABLED : WildfireLocalization.DISABLED));
-//					ref.btnAutomaticSync.setActive(CloudSync.isEnabled());
-//					ref.btnAutomaticSync.setMessage(Text.translatable("wildfire_gender.cloud.automatic", CloudSync.isEnabled() ? (GlobalConfig.INSTANCE.get(GlobalConfig.AUTOMATIC_CLOUD_SYNC) ? WildfireLocalization.ENABLED : WildfireLocalization.DISABLED) : WildfireLocalization.OFF));
-//					ref.btnSyncNow.visible = GlobalConfig.INSTANCE.get(GlobalConfig.CLOUD_SYNC_ENABLED);
-				}));
+            Text.translatable("wildfire_gender.pronoun.status", Pronoun.SHE, Objects.equals(pronouns, "SHE") ? WildfireLocalization.ENABLED : WildfireLocalization.DISABLED),
+            button -> {
+                //Doesn't do anything
+            }));
+        this.addDrawableChild(new WildfireButton(xPos, yPos, 157, 20,
+            Text.translatable("wildfire_gender.pronoun.status", Pronoun.HE, Objects.equals(pronouns, "HE") ? WildfireLocalization.ENABLED : WildfireLocalization.DISABLED),
+            button -> {
+                //Doesn't do anything
+            }));
+        this.addDrawableChild(new WildfireButton(xPos, yPos, 157, 20,
+            Text.translatable("wildfire_gender.pronoun.status", Pronoun.THEY, Objects.equals(pronouns, "THEY") ? WildfireLocalization.ENABLED : WildfireLocalization.DISABLED),
+            button -> {
+                //Doesn't do anything
+            }));
+        this.addDrawableChild(new WildfireButton(xPos, yPos, 157, 20,
+            Text.translatable("wildfire_gender.pronoun.status", Pronoun.IT, Objects.equals(pronouns, "IT") ? WildfireLocalization.ENABLED : WildfireLocalization.DISABLED),
+            button -> {
+                //Doesn't do anything
+            }));
 
         super.init();
     }
+
+    @Override
+	public void renderBackground(DrawContext ctx, int mouseX, int mouseY, float delta) {
+		this.renderInGameBackground(ctx);
+		ctx.drawTexture(RenderLayer::getGuiTextured, BACKGROUND, (this.width - 172) / 2, (this.height - 124) / 2, 0, 0, 172, 144, 256, 256);
+	}
+
+	@Override
+	public void render(DrawContext ctx, int mouseX, int mouseY, float delta) {
+        if (client == null || client.world == null) return;
+        super.render(ctx, mouseX, mouseY, delta);
+
+        int x = this.width / 2;
+        int y = this.height / 2;
+        y -= 47;
+
+        GuiUtils.drawScrollableTextWithoutShadow(GuiUtils.Justify.LEFT, ctx, textRenderer, getTitle(),
+                x - 79, y - 12, x - 79 + 141, y - 11 + 10, 4473924);
+        GuiUtils.drawScrollableTextWithoutShadow(GuiUtils.Justify.LEFT, ctx, textRenderer, Text.translatable("wildfire_gender.cloud.status_log"),
+                x - 79, y + 47, x - 79 + 95, y + 48 + 10, 4473924);
+
+        for (int i = SyncLog.SYNC_LOG.size() - 1; i >= 0; i--) {
+            int reverseIndex = SyncLog.SYNC_LOG.size() - 1 - i;
+            var entry = SyncLog.SYNC_LOG.get(i);
+
+            if (reverseIndex < 6) {
+                int ey = y + 110 - (reverseIndex * 10);
+                GuiUtils.drawScrollableTextWithoutShadow(GuiUtils.Justify.LEFT, ctx, textRenderer, entry.text(),
+                        x - 78, ey, x - 78 + 156, ey + 10, entry.color());
+            }
+        }
+    }
+
+    @Override
+	public void close() {
+		GlobalConfig.INSTANCE.save();
+		super.close();
+	}
 }
