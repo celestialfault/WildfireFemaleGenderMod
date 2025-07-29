d1de9b1d9cc9a6fdb7ed5ad74f800a80342df36c
celeste <hi@celestialfault.dev>
2025-07-28 19:48:33 -0600

    revert this change

--- src/main/java/com/wildfire/main/WildfireEventHandler.java
+++ src/main/java/com/wildfire/main/WildfireEventHandler.java
@@ -95,8 +95,7 @@ public final class WildfireEventHandler {
 			// this has to be wrapped in a lambda to ensure that a dedicated server won't crash during startup
 			// while executing this static block
 			CONFIG_KEYBIND = Util.make(() -> {
-				// FIXME this now conflicts with the Quick Actions key from vanilla as of 1.21.6-pre1
-				KeyBinding keybind = new KeyBinding("key.wildfire_gender.gender_menu", GLFW.GLFW_KEY_H, "category.wildfire_gender.generic");
+				KeyBinding keybind = new KeyBinding("key.wildfire_gender.gender_menu", GLFW.GLFW_KEY_G, "category.wildfire_gender.generic");
 				KeyBindingHelper.registerKeyBinding(keybind);
 				return keybind;
 			});
